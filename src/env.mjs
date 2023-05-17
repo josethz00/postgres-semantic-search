ad0e0d9a2246bf35f2697b990f4b177b55484797
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-17 12:17:44 -0300

    refactor: env mjs

--- src/env.mjs
+++ src/env.mjs
@@ -9,6 +9,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
+    OPEN_AI_API_KEY: z.string().min(1),
   },
 
   /**
@@ -27,6 +28,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -10,7 +10,23 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  getAll: publicProcedure.input(
+    z.object({
+      title: z.string()
+    })
+  ).query(({ ctx, input }) => {
+    return ctx.db.query(
+      `
+      SELECT id, title,
+        ts_rank(tsvector_title, to_tsquery('english', ${input.title})) AS title_rank,
+        ts_rank(tsvector_tags, to_tsquery('english', ${input.title})) AS tags_rank,
+        vector <> ARRAY[0.1, 0.2, ..., 0.512] AS distance
+      FROM articles
+      WHERE tsvector_title @@ to_tsquery('english', ${input.title})
+      OR tsvector_tags @@ to_tsquery('english', ${input.title})
+      ORDER BY (title_rank + tags_rank) DESC, distance ASC
+      LIMIT 10;
+      `
+    )
   }),
 });
