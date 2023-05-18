2a5968688abf5888af1c34d0e414de22fb6d8123
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-18 13:04:23 -0300

    fix: tags remove surrounding quotes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,37 +59,34 @@ const Home: NextPage = () => {
           </div>
           {/* Add the search results */}
           <div className="flex flex-col gap-2 mt-4 max-w-lg w-full">
-  {search.data?.rows.map((result) => (
-    <Link
-      key={result.id}
-      href={result.url as string}
-      className="hover:bg-white/10 mb-3 border border-white/10 rounded-md bg-white/5 py-2"
-    >
-      <p className="text-white ml-3 text-xl line-clamp-3">{result.title}</p>
-      {/* Display tags in a blogpost style */}
-      <div className="flex flex-wrap ml-3">
-        {result.tags[0].replace(/{/g, '').replace(/}/g, '').split(',').map((tag, index) => (
-          <span key={index} className="m-1 px-2 py-1 bg-blue-500 text-white rounded text-sm">{tag}</span>
-        ))}
-      </div>
-
-      {/* Visual representation of distance_content */}
-      <div className="my-3 mx-3 h-2 bg-gray-200 rounded-full">
-        <div className="h-full text-center text-xs text-white bg-green-500 rounded-full" style={{width: `${(1 - result.distance_content) * 100}%`}}></div>
-      </div>
-
-      <p className="text-white ml-3 text-sm">{((1 - result.distance_content) * 100).toFixed(2)}% match</p>
-      <div className="flex items-center gap-2 px-4 py-2 rounded-md">
-        <span className="text-white/50 line-clamp-3">{result.content}</span>
-      </div>
-    </Link>
-  ))}
-</div>
+            {search.data?.rows.map((result) => (
+              <Link
+                key={result.id}
+                href={result.url as string}
+                className="hover:bg-white/10 mb-3 border border-white/10 rounded-md bg-white/5 py-2"
+              >
+                <p className="text-white ml-3 text-xl line-clamp-3">{result.title}</p>
+                {/* Display tags in a blogpost style */}
+                <div className="flex flex-wrap ml-3">
+                  {result.tags[0].replace(/{/g, '').replace(/}/g, '').split(',').map((tag, index) => (
+                    <span key={index} className="m-1 px-2 py-1 bg-blue-500 text-white rounded text-sm"># {tag.replace(/"/g, '').trim()}</span>
+                  ))}
+                </div>
 
 
+                {/* Visual representation of distance_content */}
+                <div className="my-3 mx-3 h-2 bg-gray-200 rounded-full">
+                  <div className="h-full text-center text-xs text-white bg-green-500 rounded-full" style={{width: `${(1 - result.distance_content) * 100}%`}}></div>
+                </div>
 
+                <p className="text-white ml-3 text-sm">{((1 - result.distance_content) * 100).toFixed(2)}% match</p>
+                <div className="flex items-center gap-2 px-4 py-2 rounded-md">
+                  <span className="text-white/50 line-clamp-3">{result.content}</span>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
-
       </main>
     </>
   );
